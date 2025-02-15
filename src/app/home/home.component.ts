f2a8fb9a5435898234b96951205d956f406246a8
hanush-kumar <hanushkumar.su@gmail.com>
2025-02-15 06:12:39 +0000

    greeting component implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from "@angular/core";
+import { GreetingComponent } from "../components/greeting/greeting.component";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  selector: "app-home",
+  imports: [GreetingComponent],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-
+  homeMessage = signal("Hello World!");
 }
